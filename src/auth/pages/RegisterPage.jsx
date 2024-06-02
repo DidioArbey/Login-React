2787b43127ebf9c2171df8466745923bdbfcab6b
DidioArbey <didioarbey@gmail.com>
2024-06-02 17:02:03 -0500

    layout registes

--- src/auth/pages/RegisterPage.jsx
+++ src/auth/pages/RegisterPage.jsx
@@ -1,6 +1,60 @@
+import { Link as RouterLink } from 'react-router-dom'
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayaout } from '../layout/AuthLayaout'
+
 
 export const RegisterPage = () => {
   return (
-    <div>RegisterPage</div>
+    <AuthLayaout title='Crear Cuenta'>
+      <form>
+        <Grid container>
+          <Grid item xs={12} sx={{ mt: 2 }} >
+            <TextField
+              label="Nombre Completoreo"
+              type="text"
+              placeholder="Nombre Completo"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }} >
+            <TextField
+              label="Correo"
+              type="email"
+              placeholder="correo@ejemplo.com"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid item xs={12} sx={{ mt: 2 }}>
+            <TextField
+              label="Contraseña"
+              type="password"
+              placeholder="contraseña"
+              fullWidth
+            />
+          </Grid>
+
+          <Grid container spacing={2} sx={{ mb: 2, mt: 1 }}>
+            <Grid item xs={12} >
+              <Button variant='contained' fullWidth>
+                Crear Cuenta
+              </Button>
+            </Grid>
+          </Grid>
+
+          <Grid container direction='row' justifyContent='end'>
+            <Typography sx={{mr:1}} >¿Ya tienes cuenta?</Typography>
+            <Link component={RouterLink} color='inherit' to="/auth/login" >
+              Ingresar
+            </Link>
+          </Grid>
+
+        </Grid>
+      </form>
+
+    </AuthLayaout>
+
   )
 }
