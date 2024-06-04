e0086c10d6c65db45321261b0cb3921b20eb393b
DidioArbey <didioarbey@gmail.com>
2024-06-03 19:41:04 -0500

    viwes jornal app

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,13 +1,13 @@
 
 import { Typography } from "@mui/material"
 import { JournalLayout } from "../layout/JournalLayout"
+import { NothingSelectedView } from "../views"
 
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
-      <Typography>Adipisicing aliquip in et proident ad cupidatat deserunt nulla irure ex consequat labore ex voluptate. Laboris irure ullamco aliqua proident est reprehenderit ullamco nostrud est labore fugiat eu. Sint laborum voluptate ullamco in nostrud veniam labore nulla eu in. Culpa ex labore consectetur enim id irure sint eu veniam proident excepteur nulla eiusmod reprehenderit. Laborum cillum aliquip reprehenderit cillum consectetur mollit. Pariatur commodo aliquip ullamco quis ea laborum in reprehenderit anim velit adipisicing in. Irure est ipsum velit id.</Typography>
-
+      <NothingSelectedView/>
     </JournalLayout>
 
   )
