2d61839f3bf25b01e490b616d8593d766d477dd3
DidioArbey <didioarbey@gmail.com>
2024-06-11 23:21:11 -0500

    checking auth

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,9 +1,15 @@
 import { Route, Routes } from "react-router-dom"
 import { AuthRoutes } from "../auth/routes/AuthRoutes"
 import { JournalRouts } from "../journal/routes/JournalRouts"
+import { useSelector } from "react-redux"
+import { CheckingAuth } from "../ui/components/CheckingAuth"
 
 
 export const AppRouter = () => {
+  const { status } = useSelector ( state => state.auth );
+  if (status === 'checking'){
+    return <CheckingAuth />
+  }
   return (
     <Routes>
         {/* login y registro */}
--- src/store/auth/authSlice.js
+++ src/store/auth/authSlice.js
@@ -3,7 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 export const authSlice = createSlice({
     name: 'auth',
     initialState: {
-        status: 'not-authenticated',//'not-authenticated', 'authenticad','checking'
+        status: 'checking',//'not-authenticated', 'authenticad','checking'
         uid: null,
         email: null,
         diplayName: null,
